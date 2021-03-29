2246911debed3c0aa66ae38d05f082ddb402a325
Yulin Zhang <yulin.zhang@laas.fr>
2021-03-28 21:08:03 -0500

    add other experiment

--- aamas_exp_makefile.sh
+++ aamas_exp_makefile.sh
@@ -6,18 +6,32 @@ CPU_NUM=1
 LR=5e-5
 NUM_ROLLOUTS=2
 
-echo "*******************************************************************************"
-echo "run a centralized experiment to train agents with original reward, number of departed vehicles, average speeds" 
-echo "*******************************************************************************"
+echo "**********************************************************************************************************"
+echo "run a centralized experiment on simple merge to train agents with original reward, outflow, average speeds" 
+echo "**********************************************************************************************************"
 
 echo "1st exp: train agent with original reward"
 ${RUN} ${PPO_RUNNER} --benchmark_name merge4_Sim --num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
 
-echo "2nd exp: train agent with reward as the number of departing vehicles"
+echo "2nd exp: train agent with reward as the number of departing vehicles (outflow)"
 ${RUN} ${PPO_RUNNER} --benchmark_name merge4_Sim_Arrive --num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
 
 echo "3rd exp: train agent with reward as the average speeds of vehicles in the network"
 ${RUN} ${PPO_RUNNER} --benchmark_name merge4_Sim_AvgVel --num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
 
-#python rllib/ppo_runner.py --benchmark_name merge4_Sim --num_cpus number_of_cores --lr 5e-5 --num_rollouts 20
+echo "***********************************************************************************************"
+echo "run a centralized experiment on I696 to train agents with outflow, average speeds from scratch" 
+echo "***********************************************************************************************"
 
+echo "1st exp: train with outflow reward in window from scratch"
+${RUN} ${PPO_RUNNER} --benchmark_name 1merge_Window_transfer_Arrive --num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
+
+echo "2nd exp: train with avg speed reward in window from scratch"
+${RUN} ${PPO_RUNNER} --benchmark_name 1merge_Window_transfer_AvgVel --num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
+
+echo "3rd exp: train with outflow reward in the entire network from scratch"
+${RUN} ${PPO_RUNNER} --benchmark_name 1merge_horizon2000_warmup0_simstep05_flow2000_merge200_dePart10_Arrive --num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
+
+
+echo "4th exp: train with avg speed reward in the entire network from scratch"
+${RUN} ${PPO_RUNNER} --benchmark_name 1merge_horizon2000_warmup0_simstep05_flow2000_merge200_dePart10_AvgVel g-num_cpus ${CPU_NUM} --lr ${LR} --num_rollouts ${NUM_ROLLOUTS} 
