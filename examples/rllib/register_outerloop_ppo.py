c0979d7f23e9dc51586b9f0bc9ad725a5d6c8181
Yulin Zhang <yulin.zhang@laas.fr>
2021-06-02 22:14:51 -0500

    register outerloop ppo

---
+++ examples/rllib/register_outerloop_ppo.py
@@ -0,0 +1,13 @@
+import ray
+try:
+    from ray.rllib.agents.agent import ALGORITHMS
+    except ImportError:
+        from ray.rllib.agents.registry import ALGORITHMS
+
+def _import_outerloop_ppo():
+    from examples.rllib import outerloop_ppo
+    return outerloop_ppo.outerloop_PPOTrainer, outerloop_ppo.DEFAULT_CONFIG
+
+def register_outerloop_ppo():
+    ALGORITHMS["outerloop_ppo"]=_import_outerloop_ppo        
+
