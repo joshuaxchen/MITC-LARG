ff36472a9565c652e1bffcbb20e6970fa76cb87f
flow_user <william.a.macke@gmail.com>
2021-06-04 19:29:16 -0500

    update evaluation

--- shell/random_inflow_multiagent.sh
+++ shell/random_inflow_multiagent.sh
@@ -7,8 +7,8 @@ export RAY_MEMORY_MONITOR_ERROR_THRESHOLD=0.8
 #python3 examples/rllib/multiagent_exps/linearPPO_multiagent_merge4_Merge4_Collaborate_lrschedule.py 
 
 
-python3 ../examples/rllib/multiagent_exps/random_inflow_multiagent_merge4_Merge4_Collaborate_lrschedule.py --random_main_inflow 2000 1900 --random_merge_inflow 200 
-python3 ../examples/rllib/multiagent_exps/random_inflow_multiagent_merge4_Merge4_Collaborate_lrschedule.py --random_main_inflow 2000 1900 1800 --random_merge_inflow 200 
+#nohup python3 ../examples/rllib/multiagent_exps/random_inflow_multiagent_merge4_Merge4_Collaborate_lrschedule.py --random_main_inflow 2000 1900 --random_merge_inflow 200 
+nohup python3 ../examples/rllib/multiagent_exps/random_inflow_multiagent_merge4_Merge4_Collaborate_lrschedule.py --random_main_inflow 2000 1900 1800 --random_merge_inflow 200 
 #python3 ../examples/rllib/multiagent_exps/random_inflow_multiagent_merge4_Merge4_Collaborate_lrschedule.py --random_main_inflow 2000 1900 1800 1700 --random_merge_inflow 200 
 #python3 ../examples/rllib/multiagent_exps/random_inflow_multiagent_merge4_Merge4_Collaborate_lrschedule.py --random_main_inflow 2000 1900 1800 1700 1600 --random_merge_inflow 200 
 
