9ba70446122c53e43ccce76df0a23cf8516d9d47
Yulin Zhang <yulin@cs.utexas.edu>
2021-06-16 17:51:10 -0500

    update

--- exp_shell/evaluate/adaptive_headway.sh
+++ exp_shell/evaluate/adaptive_headway.sh
@@ -1,7 +1,7 @@
 TRAIN_DIR_1=/home/users/flow_user/ray_results/yulin_adaptive_headway_eta1_0.9_eta2_0.1/continuous_adaptive_headway_avp10_main2000_merge200_maxheadway20
 TRAIN_DIR_2=/home/users/flow_user/ray_results/yulin_adaptive_headway_eta1_0.9_eta2_0.1/adaptive_discrete_action_avp10_main2000_merge200_maxheadway20
 TRAIN_DIR_3=/home/users/flow_user/ray_results/yulin_adaptive_headway_eta1_0.9_eta2_0.1/adaptive_headway_count_ahead_fixed_action_avp10_main2000_merge200_maxheadway20
-TRAIN_DIR_4=/home/users/flow_user/ray_results/yulin_adaptive_headway_eta1_0.9_eta2_0.1/adaptive_headway_continous_act_penality_main2000_merge200_maxheadway10
+TRAIN_DIR_4=~/ray_results/yulin_adaptive_headway_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4AdaptiveHeadway-v0_6dae2_00000_0_2021-06-15_16-34-13
 #TRAIN_DIR_30=/home/users/flow_user/ray_results/yulin_multiagent_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/merge4_highway2000_merge200_avp_30
 #TRAIN_DIR_50=/home/users/flow_user/ray_results/yulin_multiagent_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/merge4_highway2000_merge200_avp_50
 #TRAIN_DIR_70=/home/users/flow_user/ray_results/yulin_multiagent_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/merge4_highway2000_merge200_avp_70
@@ -25,19 +25,6 @@ echo "set python path: $PYTHONPATH"
 
 MERGE_INFLOW=200
 
-#for MAIN_INFLOW in 1300 1400 1500 1600 1700 1800 1900 2000 
-#do
-#	let MAIN_RL_INFLOW=MAIN_INFLOW/10
-#	let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-#	echo $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW 
-#	python3 flow/visualize/new_rllib_visualizer.py \
-#		$TRAIN_DIR_Binary \
-#       		$CHCKPOINT \
-#		--render_mode no_render \
-#		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
-#		> ./exp_results/adaptive_headway/binary/main${MAIN_INFLOW}_merge200.txt &
-#done
-
 for AVP in 2 4 6 8 10 20 30 #40 50 60 70 80 100
 do
 	python3 $VISUALIZER $TRAIN_DIR_4 $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/adaptive_headway/continuous_penality/merge4_2000_200_TAVP_10_EAVP_${AVP}.txt &
@@ -49,76 +36,6 @@ for AVP in 40 50 60 70 80 100
 do
 	python3 $VISUALIZER $TRAIN_DIR_4 $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/adaptive_headway/continuous_penality/merge4_2000_200_TAVP_10_EAVP_${AVP}.txt &
 done
-
-
-
-#for MAIN_INFLOW in 1300 1400 1500 1600 1700 1800 1900 2000 
-#do
-#	let MAIN_RL_INFLOW=MAIN_INFLOW/10
-#	let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-#	echo $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW 
-#	python3 flow/visualize/new_rllib_visualizer.py \
-#		$TRAIN_DIR_1 \
-#       		$CHCKPOINT \
-#		--render_mode no_render \
-#		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
-#		> ./exp_results/adaptive_headway/continous/main${MAIN_INFLOW}_merge200.txt &
-#done
-#
-#wait 
-
-#for MAIN_INFLOW in 1300 1400 1500 1600 1700 1800 1900 2000 
-#do
-#	let MAIN_RL_INFLOW=MAIN_INFLOW/10
-#	let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-#	echo $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW 
-#	python3 flow/visualize/new_rllib_visualizer.py \
-#		$TRAIN_DIR_2 \
-#       		$CHCKPOINT \
-#		--render_mode no_render \
-#		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
-#		> ./exp_results/adaptive_headway/discrete/main${MAIN_INFLOW}_merge200.txt &
-#done
-#
-#wait
-#
-#for MAIN_INFLOW in 1300 1400 1500 1600 1700 1800 1900 2000 
-#do
-#	let MAIN_RL_INFLOW=MAIN_INFLOW/10
-#	let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-#	echo $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW 
-#	python3 flow/visualize/new_rllib_visualizer.py \
-#		$TRAIN_DIR_3 \
-#       		$CHCKPOINT \
-#		--render_mode no_render \
-#		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
-#		> ./exp_results/adaptive_headway/discrete_countahead/main${MAIN_INFLOW}_merge200.txt &
-#done
-
-
-#for AVP in 2 4 6 8 10 30 50 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_30 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av30/merge4_2000_200_TAVP_30_EAVP_${AVP}.txt
-#done
-
-#for AVP in 10 20 30 40 50 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_50 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av50/merge4_2000_200_TAVP_50_EAVP_${AVP}.txt
-#done
-#
-#for AVP in 10 20 30 40 50 60 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_70 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av70/merge4_2000_200_TAVP_70_EAVP_${AVP}.txt
-#done
-#
-#for AVP in 10 20 30 40 50 60 70 80 90 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_90 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av90/merge4_2000_200_TAVP_90_EAVP_${AVP}.txt
-#done
-
-
-
-
 
 
 
