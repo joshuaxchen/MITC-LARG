31668a3b509920908bcf95a6b39557255c358346
Yulin Zhang <ylzhang0008@gmail.com>
2021-07-09 00:09:24 -0500

    update shell fix issue

--- exp_shell/evaluate/aamas_models.sh
+++ exp_shell/evaluate/aamas_models.sh
@@ -4,59 +4,59 @@ declare -A TRAIN_DIR
 declare -A MARK 
 
 
-TRAIN_DIR[1]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp10_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_d59fc_00000_0_2021-06-30_12-08-48'
+TRAIN_DIR[1]=${HOME}/ray_results/yulin_multiagent_Even_Avp10_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_d59fc_00000_0_2021-06-30_12-08-48
 MARK[1]='1650_200_10'
 
-TRAIN_DIR[2]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp10_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_1cf3e_00000_0_2021-06-30_18-51-39' 
+TRAIN_DIR[2]=${HOME}/ray_results/yulin_multiagent_Even_Avp10_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_1cf3e_00000_0_2021-06-30_18-51-39 
 MARK[2]='2000_200_10'
 
 # AVP30
-TRAIN_DIR[3]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp30_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_ee3b1_00000_0_2021-06-30_18-43-11'
+TRAIN_DIR[3]=${HOME}/ray_results/yulin_multiagent_Even_Avp30_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_ee3b1_00000_0_2021-06-30_18-43-11
 MARK[3]='1650_200_30'
 
-TRAIN_DIR[4]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp30_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_d1a31_00000_0_2021-07-01_00-47-28'
+TRAIN_DIR[4]=${HOME}/ray_results/yulin_multiagent_Even_Avp30_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_d1a31_00000_0_2021-07-01_00-47-28
 MARK[4]='1850_200_30'
 
 
-TRAIN_DIR[5]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp30_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_7c1f0_00000_0_2021-07-01_07-25-56'
+TRAIN_DIR[5]=${HOME}/ray_results/yulin_multiagent_Even_Avp30_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_7c1f0_00000_0_2021-07-01_07-25-56
 MARK[5]='2000_200_30'
 
 
 # AVP50
-TRAIN_DIR[6]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp50_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_08b48_00000_0_2021-07-01_14-03-34'
+TRAIN_DIR[6]=${HOME}/ray_results/yulin_multiagent_Even_Avp50_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_08b48_00000_0_2021-07-01_14-03-34
 MARK[6]='1650_200_50'
 
 
-TRAIN_DIR[7]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp50_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_9a8de_00000_0_2021-07-01_21-45-47'
+TRAIN_DIR[7]=${HOME}/ray_results/yulin_multiagent_Even_Avp50_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_9a8de_00000_0_2021-07-01_21-45-47
 MARK[7]='1850_200_50'
 
 
-TRAIN_DIR[8]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp50_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_77c29_00000_0_2021-07-02_05-58-43'
+TRAIN_DIR[8]=${HOME}/ray_results/yulin_multiagent_Even_Avp50_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_77c29_00000_0_2021-07-02_05-58-43
 MARK[8]='2000_200_50'
 
 
 # AVP80
-TRAIN_DIR[9]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp80_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_f1df5_00000_0_2021-07-02_14-37-32'
+TRAIN_DIR[9]=${HOME}/ray_results/yulin_multiagent_Even_Avp80_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_f1df5_00000_0_2021-07-02_14-37-32
 MARK[9]='1650_200_80'
 
-TRAIN_DIR[10]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp80_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_811d5_00000_0_2021-07-03_00-50-00'
+TRAIN_DIR[10]=${HOME}/ray_results/yulin_multiagent_Even_Avp80_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_811d5_00000_0_2021-07-03_00-50-00
 MARK[10]='1850_200_80'
 
-TRAIN_DIR[11]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp80_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_ba955_00000_0_2021-07-03_11-43-00'
+TRAIN_DIR[11]=${HOME}/ray_results/yulin_multiagent_Even_Avp80_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_ba955_00000_0_2021-07-03_11-43-00
 MARK[11]='2000_200_80'
 
-TRAIN_DIR[12]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp100_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_1d245_00000_0_2021-07-05_11-43-23'
+TRAIN_DIR[12]=${HOME}/ray_results/yulin_multiagent_Even_Avp100_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_1d245_00000_0_2021-07-05_11-43-23
 MARK[12]='1650_200_100'
 
-TRAIN_DIR[13]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp100_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_8a7f3_00000_0_2021-07-01_16-08-53'
+TRAIN_DIR[13]=${HOME}/ray_results/yulin_multiagent_Even_Avp100_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_8a7f3_00000_0_2021-07-01_16-08-53
 MARK[13]='2000_200_100'
 
 #####new##########
-TRAIN_DIR[14]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp100_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_d6d8e_00000_0_2021-07-07_12-07-41'
+TRAIN_DIR[14]=${HOME}/ray_results/yulin_multiagent_Even_Avp100_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_d6d8e_00000_0_2021-07-07_12-07-41
 MARK[14]='1850_200_100'
 # neymar
 
-TRAIN_DIR[15]='/home/users/flow_user/ray_results/yulin_multiagent_Even_Avp10_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_e7ee4_00000_0_2021-07-07_23-49-40'
+TRAIN_DIR[15]=${HOME}/ray_results/yulin_multiagent_Even_Avp10_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_e7ee4_00000_0_2021-07-07_23-49-40
 MARK[15]='1850_200_10'
 # commander 
 
@@ -81,26 +81,25 @@ echo "************************************************************"
 #echo ${TRAIN_DIR[*]}
 NUM=0
 
-for I in 14 15
+
+mkdir ${FLOW_DIR}/exp_results/aamas_models
+for I in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 	echo "${TRAIN_DIR[$I]}"
-	mkdir ~/yulin/MITC-LARGE/exp_results/aamas_models/${MARK[$I]}
+	mkdir ${FLOW_DIR}/exp_results/aamas_models/${MARK[$I]}
 	for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
 	do
+		echo "work"
 		echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
 		python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models/${MARK[$I]}/merge4_${MARK[$I]}_EAVP_${AVP}.txt &
-		#let NUM=(NUM+1)
-		#echo $NUM
-		#if (($NUM>10))
-		#then
-		#	let NUM=(NUM-10)
-		#	echo $NUM
-		#	wait
-		#fi
 	done
-	#wait
+	if ((I == 4 || I==8 || I==12)); then
+		wait
+	fi
+
 done
 
+# || test $I -eq 8 || test $I -eq 12
 #for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
 #do
 #	python3 $VISUALIZER $TRAIN_DIR_1 $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models/1650_200_10/merge4_1650_200_TAVP_10_EAVP_${AVP}.txt &
--- exp_shell/evaluate/aamas_models_random.sh
+++ exp_shell/evaluate/aamas_models_random.sh
@@ -4,61 +4,61 @@ declare -A TRAIN_DIR
 declare -A MARK 
 
 
-TRAIN_DIR[1]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp10_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_b6165_00000_0_2021-07-03_10-52-46'
+TRAIN_DIR[1]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp10_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_b6165_00000_0_2021-07-03_10-52-46
 MARK[1]='1650_200_10'
 
-TRAIN_DIR[2]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp10_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_803da_00000_0_2021-07-03_16-56-20' 
+TRAIN_DIR[2]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp10_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_803da_00000_0_2021-07-03_16-56-20 
 MARK[2]='1850_200_10'
 
 # AVP30
-TRAIN_DIR[3]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp10_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_74d88_00000_0_2021-07-03_21-20-52'
+TRAIN_DIR[3]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp10_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_74d88_00000_0_2021-07-03_21-20-52
 MARK[3]='2000_200_10'
 ###############
 
-TRAIN_DIR[4]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_a9c72_00000_0_2021-07-04_01-25-44'
+TRAIN_DIR[4]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_a9c72_00000_0_2021-07-04_01-25-44
 MARK[4]='1650_200_30'
 
 
-TRAIN_DIR[5]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_0a35b_00000_0_2021-07-04_07-54-59'
+TRAIN_DIR[5]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_0a35b_00000_0_2021-07-04_07-54-59
 MARK[5]='1850_200_30'
 
 
-TRAIN_DIR[6]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_740c0_00000_0_2021-07-04_14-31-39'
+TRAIN_DIR[6]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_740c0_00000_0_2021-07-04_14-31-39
 MARK[6]='2000_200_30'
 
 
-TRAIN_DIR[7]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp50_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_6328a_00000_0_2021-07-04_21-04-53'
+TRAIN_DIR[7]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp50_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_6328a_00000_0_2021-07-04_21-04-53
 MARK[7]='1650_200_50'
 
-TRAIN_DIR[8]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp50_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_6fb5c_00000_0_2021-07-05_05-40-37'
+TRAIN_DIR[8]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp50_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_6fb5c_00000_0_2021-07-05_05-40-37
 MARK[8]='1850_200_50'
 
 
-TRAIN_DIR[9]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp50_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_3688f_00000_0_2021-07-05_14-07-16'
+TRAIN_DIR[9]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp50_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_3688f_00000_0_2021-07-05_14-07-16
 MARK[9]='2000_200_50'
 
 ##############
-TRAIN_DIR[10]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp80_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_5a6ae_00000_0_2021-07-04_01-52-09'
+TRAIN_DIR[10]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp80_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_5a6ae_00000_0_2021-07-04_01-52-09
 MARK[10]='1650_200_80'
 
 
-TRAIN_DIR[11]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp80_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_2ad89_00000_0_2021-07-04_11-52-07'
+TRAIN_DIR[11]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp80_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_2ad89_00000_0_2021-07-04_11-52-07
 MARK[11]='1850_200_80'
 
 ########new############
-TRAIN_DIR[12]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp80_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_cb311_00000_0_2021-07-07_12-50-18'
+TRAIN_DIR[12]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp80_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_cb311_00000_0_2021-07-07_12-50-18
 MARK[12]='2000_200_80'
 # already have
 
-TRAIN_DIR[13]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp100_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_e4471_00000_0_2021-07-07_15-07-01'
+TRAIN_DIR[13]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp100_Main1650_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_e4471_00000_0_2021-07-07_15-07-01
 MARK[13]='1650_200_100'
 # already have
 
-TRAIN_DIR[14]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp100_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_54bf7_00000_0_2021-07-07_16-28-54'
+TRAIN_DIR[14]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp100_Main1850_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_54bf7_00000_0_2021-07-07_16-28-54
 MARK[14]='1850_200_100'
 # dr-wily
 
-TRAIN_DIR[15]='/home/users/flow_user/ray_results/yulin_random_placement_multiagent_Even_Avp100_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_56c3a_00000_0_2021-07-07_23-38-27'
+TRAIN_DIR[15]=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp100_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_56c3a_00000_0_2021-07-07_23-38-27
 MARK[15]='2000_200_100'
 # dr-light 
 
@@ -83,17 +83,56 @@ echo "************************************************************"
 #echo ${TRAIN_DIR[*]}
 NUM=0
 
-mkdir ~/yulin/MITC-LARGE/exp_results/aamas_models_random
-for I in 12 13 14 15
+mkdir ${EXP_FOLDER}/aamas_models_random
+
+for I in 1 2 3 4 
 do
 	echo "${TRAIN_DIR[$I]}"
-	mkdir ~/yulin/MITC-LARGE/exp_results/aamas_models_random/${MARK[$I]}
+	mkdir ${EXP_FOLDER}/aamas_models_random/${MARK[$I]}
 	for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
 	do
 		echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
-		python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models/${MARK[$I]}/merge4_${MARK[$I]}_EAVP_${AVP}.txt &
+		python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models_random/${MARK[$I]}/merge4_${MARK[$I]}_EAVP_${AVP}.txt &
 	done
-	#wait
+done
+
+wait 
+
+for I in 5 6 7 8 
+do
+	echo "${TRAIN_DIR[$I]}"
+	mkdir ${EXP_FOLDER}/aamas_models_random/${MARK[$I]}
+	for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
+	do
+		echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
+		python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models_random/${MARK[$I]}/merge4_${MARK[$I]}_EAVP_${AVP}.txt &
+	done
+done
+
+wait 
+
+for I in 9 10 11 12 
+do
+	echo "${TRAIN_DIR[$I]}"
+	mkdir ${EXP_FOLDER}/aamas_models_random/${MARK[$I]}
+	for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
+	do
+		echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
+		python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models_random/${MARK[$I]}/merge4_${MARK[$I]}_EAVP_${AVP}.txt &
+	done
+done
+
+wait 
+
+for I in 13 14 15
+do
+	echo "${TRAIN_DIR[$I]}"
+	mkdir ${EXP_FOLDER}/aamas_models_random/${MARK[$I]}
+	for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
+	do
+		echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
+		python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --handset_avp ${AVP} >> $EXP_FOLDER/aamas_models_random/${MARK[$I]}/merge4_${MARK[$I]}_EAVP_${AVP}.txt &
+	done
 done
 
 #for AVP in 2 4 6 8 10 20 30 40 50 60 70 80 100
