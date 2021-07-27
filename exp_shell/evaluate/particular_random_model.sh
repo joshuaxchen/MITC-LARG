3da1deb5b1ed5118006bc858adc5ff1e3a8e1a4d
Yulin Zhang <ylzhang0008@gmail.com>
2021-07-27 10:15:21 -0500

    add exp shell

--- exp_shell/evaluate/particular_random_model.sh
+++ exp_shell/evaluate/particular_random_model.sh
@@ -93,23 +93,26 @@ for I in 4 5 6 #7 8 9 10 11 12 13 14 15
 do
 	echo "${TRAIN_DIR[$I]}"
 	mkdir ${WORKING_DIR}/${MARK[$I]}
-	for MAIN_INFLOW in 2700 2800 2900 3000
-	do
-		for AVP in 1 5 10 16 20 25 30 40
+
+	for MERGE_INFLOW in 180 190 200 210 220 230 240 250
+		for MAIN_INFLOW in 2000
 		do
-			let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
-			let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-			echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
-			echo $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW
-			python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --avp_to_probability ${AVP} --handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW >> ${WORKING_DIR}/${MARK[$I]}/merge4_EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}.txt &
-			let J=J+1
-			if ((J == 20)); then
-				wait
-				let J=0
-				echo "another batch"
-			fi
+			for AVP in 1 5 10 16 20 30 
+			do
+				let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
+				let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
+				echo "evaluate" ${TRAIN_DIR[$I]} ${MARK[$I]} "on AVP ${AVP}"
+				echo $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW
+				python3 $VISUALIZER ${TRAIN_DIR[$I]} $CHCKPOINT --render_mode no_render --seed_dir $FLOW_DIR --avp_to_probability ${AVP} --handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW >> ${WORKING_DIR}/${MARK[$I]}/merge4_EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}.txt &
+				let J=J+1
+				if ((J == 20)); then
+					wait
+					let J=0
+					echo "another batch"
+				fi
+			done
 		done
-	done
+	done 
 done
 
 wait
