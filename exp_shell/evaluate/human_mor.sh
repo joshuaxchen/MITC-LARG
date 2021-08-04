dcce526735cf6765021bf7c2c55fd6f164bc0afa
Yulin Zhang <ylzhang0008@gmail.com>
2021-08-03 22:02:48 -0500

    add shell

--- exp_shell/evaluate/human_mor.sh
+++ exp_shell/evaluate/human_mor.sh
@@ -4,6 +4,10 @@ TRAIN_DIR=/home/users/yulin/ray_results/multiagent_highway_merge4_MOR_Collaborat
 TRAIN_DIR=/home/users/yulin/ray_results/multiagent_highway_merge4_MOR_Collaborate_lrschedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4CollaborateMOR-v0_96d1f_00000_0_2021-07-31_23-18-57
 
 FLOW_DIR=${PWD}/../..
+VISUALIZER=$FLOW_DIR/flow/visualize/new_rllib_visualizer.py
+EXP_FOLDER=$FLOW_DIR/exp_results
+WORKING_DIR=$EXP_FOLDER/600_200_new_mor
+
 
 CHCKPOINT=500
 
@@ -24,26 +28,32 @@ AFTER=100
 AVP=10
 
 TOTAL=1500
-LOC1=500
+LOC1=600
+END=200
 
 MAIN_INFLOW=2000
 MERGE_INFLOW=200
 AVP=10
-for MAIN_INFLOW in 2000 2100 2200 #1800 1900 #
+for END in 200 
 do
-	for DIST_BETWEEN in 200 400 600 800
+	WORKING_DIR=$EXP_FOLDER/${LOC1}_${END}_human_mor
+	mkdir ${WORKING_DIR}
+	for MAIN_INFLOW in 1800 1900 2000 2100 2200 #1800 1900 #
 	do
-		let LOC2=LOC1+DIST_BETWEEN
-		echo $LOC1 $LOC2
-		python3 ../../flow/visualize/new_rllib_visualizer.py \
-			$TRAIN_DIR \
-			$CHCKPOINT \
-			--seed_dir $FLOW_DIR \
-			--render_mode no_render \
-			--highway_len ${TOTAL} \
-			--on_ramps ${LOC1} ${LOC2} \
-			--main_merge_human_inflows ${MAIN_INFLOW} ${MERGE_INFLOW}\
-			> ../../exp_results/new_human_mor/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${LOC1}_${LOC2}.txt & 
+		for DIST_BETWEEN in 200 400 600 800
+		do
+			let LOC2=LOC1+DIST_BETWEEN
+			let TOTAL=LOC2+END
+			echo $LOC1 $LOC2
+			python3 $VISUALIZER \
+				$TRAIN_DIR \
+				$CHCKPOINT \
+				--seed_dir $FLOW_DIR \
+				--render_mode no_render \
+				--highway_len ${TOTAL} \
+				--on_ramps ${LOC1} ${LOC2} \
+				--main_merge_human_inflows ${MAIN_INFLOW} ${MERGE_INFLOW}\
+				> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${LOC1}_${LOC2}.txt & 
 	done
 done
 
