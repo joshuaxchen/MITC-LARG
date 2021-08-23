2ebc5d4736a04d15602ca5195ebc7fd74d2aff51
Yulin Zhang <ylzhang0008@gmail.com>
2021-08-23 12:59:10 -0500

    add exp shell

--- exp_shell/evaluate/lanechange.sh
+++ exp_shell/evaluate/lanechange.sh
@@ -14,7 +14,7 @@ PRESET_2=${HOME}/ray_results/multiagent_yulin2_lanechange_merge4_Full_Collaborat
 
 FLOW_DIR=${PWD}/../..
 VISUALIZER=$FLOW_DIR/flow/visualize/new_rllib_visualizer.py
-EXP_FOLDER=$FLOW_DIR/exp_results/lane_change
+EXP_FOLDER=$FLOW_DIR/exp_results/lane_change_1
 
 
 CHCKPOINT=500
@@ -64,26 +64,30 @@ do
 			do 
 				for AGGRESSIVE in 0.2 0.4 0.6 0.8 1
 				do
-					python3 $VISUALIZER \
-						$PRESET_0 \
-						$CHCKPOINT \
-						--seed_dir $FLOW_DIR \
-						--lateral_resolution 3.2 \
-						--render_mode no_render \
-						--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
-						--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
-						--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
-						--rl_lane_change 0 0 \
-						--merge_inflow ${MERGE_INFLOW} \
-						--aggressive ${AGGRESSIVE} 
-						>> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}.txt &
+					for ASSERTIVE in 0.5 1 #0.4 0.6 0.8 1
+					do
+						python3 $VISUALIZER \
+							$PRESET_0 \
+							$CHCKPOINT \
+							--seed_dir $FLOW_DIR \
+							--lateral_resolution 3.2 \
+							--render_mode no_render \
+							--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
+							--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+							--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
+							--rl_lane_change 0 0 \
+							--merge_inflow ${MERGE_INFLOW} \
+							--aggressive ${AGGRESSIVE} \
+							--assertive ${ASSERTIVE} \
+							>> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}.txt &
 
-					let J=J+1
-					if ((J == 20)); then
-						wait
-						let J=0
-						echo "another batch"
-					fi
+						let J=J+1
+						if ((J == 20)); then
+							wait
+							let J=0
+							echo "another batch"
+						fi
+					done
 				done
 			done
 		done
@@ -116,36 +120,40 @@ do
 			echo ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} 
 			echo ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}
 
-			for RIGHT_HUMAN_LANE_CHANGE in 1
+			for RIGHT_HUMAN_LANE_CHANGE in 0
 			do 
 				for AGGRESSIVE in 0.2 0.4 0.6 0.8 1
 				do
-					python3 $VISUALIZER \
-						$PRESET_1 \
-						$CHCKPOINT \
-						--seed_dir $FLOW_DIR \
-						--lateral_resolution 3.2 \
-						--render_mode no_render \
-						--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
-						--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
-						--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
-						--rl_lane_change 0 0 \
-						--merge_inflow ${MERGE_INFLOW} \
-						--aggressive ${AGGRESSIVE} \
-						>> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}.txt &
+					for ASSERTIVE in 0.5 1 #5 #0.4 0.6 0.8 1
+					do
+						python3 $VISUALIZER \
+							$PRESET_0 \
+							$CHCKPOINT \
+							--seed_dir $FLOW_DIR \
+							--lateral_resolution 3.2 \
+							--render_mode no_render \
+							--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
+							--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+							--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
+							--rl_lane_change 0 0 \
+							--merge_inflow ${MERGE_INFLOW} \
+							--aggressive ${AGGRESSIVE} \
+							--assertive ${ASSERTIVE} \
+							>> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}.txt &
 
-					let J=J+1
-					if ((J == 20)); then
-						wait
-						let J=0
-						echo "another batch"
-					fi
+						let J=J+1
+						if ((J == 20)); then
+							wait
+							let J=0
+							echo "another batch"
+						fi
+					done
 				done
 			done
 		done
 	done
 done
-#
+
 WORKING_DIR=$EXP_FOLDER/preset_2
 mkdir ${WORKING_DIR}
 
@@ -156,7 +164,7 @@ do
 		let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
 		let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
 
-		for RL_RIGHT_LEFT in 1
+		for RL_RIGHT_LEFT in 0
 		do
 			if ((RL_RIGHT_LEFT == 0)); then
 				RL_INFLOW_LEFT=0
@@ -172,30 +180,34 @@ do
 			echo ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} 
 			echo ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}
 
-			for RIGHT_HUMAN_LANE_CHANGE in 1
+			for RIGHT_HUMAN_LANE_CHANGE in 0
 			do 
 				for AGGRESSIVE in 0.2 0.4 0.6 0.8 1
 				do
-					python3 $VISUALIZER \
-						$PRESET_2 \
-						$CHCKPOINT \
-						--seed_dir $FLOW_DIR \
-						--lateral_resolution 3.2 \
-						--render_mode no_render \
-						--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
-						--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
-						--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
-						--rl_lane_change 0 0 \
-						--merge_inflow ${MERGE_INFLOW} \
-						--aggressive ${AGGRESSIVE} \
-						>> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}.txt &
+					for ASSERTIVE in 0.5 1 #5 #0.4 0.6 0.8 1
+					do
+						python3 $VISUALIZER \
+							$PRESET_0 \
+							$CHCKPOINT \
+							--seed_dir $FLOW_DIR \
+							--lateral_resolution 3.2 \
+							--render_mode no_render \
+							--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
+							--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+							--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
+							--rl_lane_change 0 0 \
+							--merge_inflow ${MERGE_INFLOW} \
+							--aggressive ${AGGRESSIVE} \
+							--assertive ${ASSERTIVE} \
+							>> ${WORKING_DIR}/EVAL_${MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}.txt &
 
-					let J=J+1
-					if ((J == 20)); then
-						wait
-						let J=0
-						echo "another batch"
-					fi
+						let J=J+1
+						if ((J == 20)); then
+							wait
+							let J=0
+							echo "another batch"
+						fi
+					done
 				done
 			done
 		done
@@ -203,42 +215,7 @@ do
 done
 
 
+
 wait 
 source ~/notification_zyl.sh
-
-#--render_mode no_render \
-#--main_merge_human_inflows $MAIN_INFLOW $MERGE_INFLOW \
-
-
-#for AVP in 2 4 6 8 10 30 50 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $HUMAN_DIR $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./exp_results/adaptive_headway/avp10/merge4_2000_200_TAVP_10_EAVP_${AVP}.txt &
-#done
-
-#for AVP in 2 4 6 8 10 30 50 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_30 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av30/merge4_2000_200_TAVP_30_EAVP_${AVP}.txt
-#done
-
-#for AVP in 10 20 30 40 50 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_50 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av50/merge4_2000_200_TAVP_50_EAVP_${AVP}.txt
-#done
-#
-#for AVP in 10 20 30 40 50 60 70 80 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_70 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av70/merge4_2000_200_TAVP_70_EAVP_${AVP}.txt
-#done
-#
-#for AVP in 10 20 30 40 50 60 70 80 90 100
-#do
-#	python3 flow/visualize/new_rllib_visualizer.py $TRAIN_DIR_90 $CHCKPOINT --render_mode no_render --handset_avp ${AVP} >> ./avp_multi_agent/av90/merge4_2000_200_TAVP_90_EAVP_${AVP}.txt
-#done
-
-
-
-
-
-
-
 
