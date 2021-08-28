19810795392e52f66571acab7fab5dc11f25f95a
Yulin Zhang <yulin@cs.utexas.edu>
2021-08-27 20:22:44 -0500

    update lane change color and aggressiveness

--- exp_shell/evaluate/lanechange.sh
+++ exp_shell/evaluate/lanechange.sh
@@ -94,6 +94,7 @@ mkdir ${WORKING_DIR}
 #	done
 #done
 
+WORKING_DIR=$EXP_FOLDER/human
 RIGHT_MAIN_INFLOW=2000
 
 for RIGHT_MAIN_INFLOW in 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
@@ -121,31 +122,35 @@ do
 
 				for RIGHT_HUMAN_LANE_CHANGE in 1
 				do 
-					for AGGRESSIVE in 0.2 0.4 0.6 0.8 1
+					for AGGRESSIVE in 1 #0.2 0.4 0.6 0.8 1
 					do
-						for ASSERTIVE in 0.5 #5 #0.4 0.6 0.8 1
+						for ASSERTIVE in 1 #0.5 #5 #0.4 0.6 0.8 1
 						do
-							python3 $VISUALIZER \
-								$PRESET_0 \
-								$CHCKPOINT \
-								--seed_dir $FLOW_DIR \
-								--lateral_resolution 3.2 \
-								--render_mode no_render \
-								--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
-								--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
-								--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
-								--rl_lane_change 0 0 \
-								--merge_inflow ${MERGE_INFLOW} \
-								--aggressive ${AGGRESSIVE} \
-								--assertive ${ASSERTIVE} \
-								>> ${WORKING_DIR}/EVAL_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}.txt &
+							for LC_PROB in 0.2 0.4 0.6 0.8 1
+							do
+								python3 $VISUALIZER \
+									$PRESET_0 \
+									$CHCKPOINT \
+									--seed_dir $FLOW_DIR \
+									--lateral_resolution 3.2 \
+									--render_mode no_render \
+									--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
+									--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+									--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
+									--rl_lane_change 0 0 \
+									--merge_inflow ${MERGE_INFLOW} \
+									--aggressive ${AGGRESSIVE} \
+									--assertive ${ASSERTIVE} \
+									--lc_probability ${LC_PROB}
+									>> ${WORKING_DIR}/EVAL_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}_${LC_PROB}.txt &
 
-							let J=J+1
-							if ((J == 20)); then
-								wait
-								let J=0
-								echo "another batch"
-							fi
+								let J=J+1
+								if ((J == 20)); then
+									wait
+									let J=0
+									echo "another batch"
+								fi
+							done
 						done
 					done
 				done
--- flow/controllers/lane_change_controllers.py
+++ flow/controllers/lane_change_controllers.py
@@ -3,6 +3,11 @@
 from flow.controllers.base_lane_changing_controller import \
     BaseLaneChangeController
 
+import random
+
+PURPLE= (128,0,128)
+YELLOW= (255,255,0) 
+GREEN= (0,255,0) 
 
 class SimLaneChangeController(BaseLaneChangeController):
     """A controller used to enforce sumo lane-change dynamics on a vehicle.
@@ -29,7 +34,6 @@ class SimpleMergeLaneChanger(BaseLaneChangeController):
     """A lane-changing model to control the amount of vehicles to do lane changing
     """
 
-    def get_lane_change_action(self, env):
     """Specify the lane change action to be performed.
 
         If discrete lane changes are being performed, the action is a direction
@@ -47,7 +51,59 @@ class SimpleMergeLaneChanger(BaseLaneChangeController):
         -------
         float or int
             requested lane change action
-        """
+    """
 
-        return 1
+    def __init__(self, veh_id, lane_change_params=None):
+        super().__init__(veh_id, lane_change_params)
+        if 'lane_change_region_start_loc' not in lane_change_params.keys():
+            print('lane_change_region_start_loc is required in lane_change_params for SimpleMergeLaneChanger')
+            exit(-1)
+        if 'lane_change_region_end_loc' not in lane_change_params.keys():
+            print('lane_change_region_end_loc is required in lane_change_params for SimpleMergeLaneChanger')
+            exit(-1)
+        if 'lane_change_probability' not in lane_change_params.keys():
+            print('lane_change_probability is required in lane_change_params for SimpleMergeLaneChanger')
+
+        self.lane_change_region_start_loc=lane_change_params['lane_change_region_start_loc']
+        self.lane_change_region_end_loc=lane_change_params['lane_change_region_end_loc']
+        self.lane_change_probability=lane_change_params['lane_change_probability']
+        # TODO: log the seed used for experiment replay
+        sampled_prob=random.random()
+        if sampled_prob<=self.lane_change_probability:
+            self.lane_change_switch=True
+        else:
+            self.lane_change_switch=False
+
+        self.prev_lane=None
+        self.changed_t=None
+
+
+    def get_lane_change_action(self, env):
+        lane_id=env.k.vehicle.get_lane(self.veh_id)
+        r, g, b, t=env.k.vehicle.get_color_t(self.veh_id)
+        if self.prev_lane is not None and self.prev_lane !=lane_id:
+            env.k.vehicle.set_color(self.veh_id, YELLOW)
+            self.changed_t=1
+        elif self.changed_t is not None: # has changed lane
+            if self.changed_t<=25:
+                b=self.changed_t*10
+                if b>255:
+                    b=255
+                env.k.vehicle.set_color(self.veh_id, (b,255,b))
+                self.changed_t+=1
+            else:
+                self.changed_t=None
+                WHITE = (255, 255, 255)
+                env.k.vehicle.set_color(self.veh_id, WHITE)
+
+        self.prev_lane=lane_id
+
+            #env.k.vehicle.set_color(self.veh_id, PURPLE)
+        if lane_id==1: # do nothing for left lane
+            self.lane_change_switch=False
+        loc=env.k.vehicle.get_x_by_id(self.veh_id)
+        if self.lane_change_switch and loc>=self.lane_change_region_start_loc and loc<=self.lane_change_region_end_loc:
+            return 1
+        else:
+            return 0
     
