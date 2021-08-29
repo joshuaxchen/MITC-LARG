07728b011deada976bbc1a48900c9a7c81e4477f
Yulin Zhang <ylzhang0008@gmail.com>
2021-08-29 11:32:36 -0500

    add shell

--- exp_shell/train/lc.sh
+++ exp_shell/train/lc.sh
@@ -16,7 +16,7 @@ AVP=10 #200 400 600 800 # 200 400 600 800 # 200 400 600 800
 let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
 let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
 
-RL_RIGHT_LEFT=0
+RL_RIGHT_LEFT=1
 if ((RL_RIGHT_LEFT == 0)); then # rl on the right
 	RL_INFLOW_LEFT=0
 	let RL_INFLOW_RIGHT=RIGHT_MAIN_INFLOW*${AVP}/100
@@ -34,9 +34,9 @@ AGGRESSIVE=1 #0.2 0.4 0.6 0.8 1
 ASSERTIVE=1 #0.5 #5 #0.4 0.6 0.8 1
 LC_PROB=0.6
 python3 ${FLOW_DIR}/examples/rllib/multiagent_exps/multiagent_lane_change_merge4_Collaborate_lrschedule.py \
-	--exp_folder_mark yulin_rl_right \
+	--exp_folder_mark yulin_rl_left \
 	--lateral_resolution 3.2 \
-	--cpu 30 \
+	--cpu 20 \
 	--to_probability \
 	--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
 	--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
