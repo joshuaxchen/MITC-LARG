008304a559ca06feb703e5893209df0d8cc04f19
Yulin Zhang <ylzhang0008@gmail.com>
2021-08-29 22:58:11 -0500

    update shell

--- exp_shell/evaluate/lanechange.sh
+++ exp_shell/evaluate/lanechange.sh
@@ -39,7 +39,7 @@ RIGHT_MAIN_INFLOW=2000
 
 for RIGHT_MAIN_INFLOW in 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
 do
-	for LEFT_MAIN_INFLOW in 1700 1800 1900 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
+	for LEFT_MAIN_INFLOW in 1600 1500 1400 1300 1200 1100 1000 #1700 1800 1900 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
 	do
 		for AVP in 10 #200 400 600 800 # 200 400 600 800 # 200 400 600 800
 		do
@@ -102,71 +102,71 @@ done
 
 
 
-WORKING_DIR=$EXP_FOLDER/preset_2
-mkdir ${WORKING_DIR}
-
-
-for RIGHT_MAIN_INFLOW in 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
-do
-	for LEFT_MAIN_INFLOW in 1700 1800 1900 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
-	do
-		for AVP in 10 #200 400 600 800 # 200 400 600 800 # 200 400 600 800
-		do
-			let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
-			let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-
-			for RL_RIGHT_LEFT in 1
-			do
-				if ((RL_RIGHT_LEFT == 0)); then # rl on the right
-					RL_INFLOW_LEFT=0
-					let RL_INFLOW_RIGHT=RIGHT_MAIN_INFLOW*${AVP}/100
-				else # otherwise, rl vehicles on the left
-					let RL_INFLOW_LEFT=LEFT_MAIN_INFLOW*${AVP}/100
-					RL_INFLOW_RIGHT=0
-				fi
-				let HUMAN_INFLOW_LEFT=LEFT_MAIN_INFLOW-RL_INFLOW_LEFT
-				let HUMAN_INFLOW_RIGHT=RIGHT_MAIN_INFLOW-RL_INFLOW_RIGHT
-				echo ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} 
-				echo ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}
-
-				for RIGHT_HUMAN_LANE_CHANGE in 1
-				do 
-					for AGGRESSIVE in 1 #0.2 0.4 0.6 0.8 1
-					do
-						for ASSERTIVE in 1 #0.5 #5 #0.4 0.6 0.8 1
-						do
-							for LC_PROB in 0.2 0.4 0.6 0.8 1
-							do
-								python3 $VISUALIZER \
-									$RL_LEFT_MODEL \
-									$CHCKPOINT \
-									--seed_dir $FLOW_DIR \
-									--lateral_resolution 3.2 \
-									--render_mode no_render \
-									--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
-									--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
-									--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
-									--rl_lane_change 0 0 \
-									--merge_inflow ${MERGE_INFLOW} \
-									--aggressive ${AGGRESSIVE} \
-									--assertive ${ASSERTIVE} \
-									--lc_probability ${LC_PROB} \
-									>> ${WORKING_DIR}/EVAL_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}_${LC_PROB}.txt &
-
-								let J=J+1
-								if ((J == 20)); then
-									wait
-									let J=0
-									echo "another batch"
-								fi
-							done
-						done
-					done
-				done
-			done
-		done
-	done
-done
+#WORKING_DIR=$EXP_FOLDER/preset_2
+#mkdir ${WORKING_DIR}
+#
+#
+#for RIGHT_MAIN_INFLOW in 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
+#do
+#	for LEFT_MAIN_INFLOW in 1600 1500 1400 1300 1200 1100 1000 #1700 1800 1900 2000  # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
+#	do
+#		for AVP in 10 #200 400 600 800 # 200 400 600 800 # 200 400 600 800
+#		do
+#			let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
+#			let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
+#
+#			for RL_RIGHT_LEFT in 1
+#			do
+#				if ((RL_RIGHT_LEFT == 0)); then # rl on the right
+#					RL_INFLOW_LEFT=0
+#					let RL_INFLOW_RIGHT=RIGHT_MAIN_INFLOW*${AVP}/100
+#				else # otherwise, rl vehicles on the left
+#					let RL_INFLOW_LEFT=LEFT_MAIN_INFLOW*${AVP}/100
+#					RL_INFLOW_RIGHT=0
+#				fi
+#				let HUMAN_INFLOW_LEFT=LEFT_MAIN_INFLOW-RL_INFLOW_LEFT
+#				let HUMAN_INFLOW_RIGHT=RIGHT_MAIN_INFLOW-RL_INFLOW_RIGHT
+#				echo ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} 
+#				echo ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}
+#
+#				for RIGHT_HUMAN_LANE_CHANGE in 1
+#				do 
+#					for AGGRESSIVE in 1 #0.2 0.4 0.6 0.8 1
+#					do
+#						for ASSERTIVE in 1 #0.5 #5 #0.4 0.6 0.8 1
+#						do
+#							for LC_PROB in 0.2 0.4 0.6 0.8 1
+#							do
+#								python3 $VISUALIZER \
+#									$RL_LEFT_MODEL \
+#									$CHCKPOINT \
+#									--seed_dir $FLOW_DIR \
+#									--lateral_resolution 3.2 \
+#									--render_mode no_render \
+#									--human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}\
+#									--rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+#									--human_lane_change ${RIGHT_HUMAN_LANE_CHANGE} 0 \
+#									--rl_lane_change 0 0 \
+#									--merge_inflow ${MERGE_INFLOW} \
+#									--aggressive ${AGGRESSIVE} \
+#									--assertive ${ASSERTIVE} \
+#									--lc_probability ${LC_PROB} \
+#									>> ${WORKING_DIR}/EVAL_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP}_${RL_RIGHT_LEFT}_${RIGHT_HUMAN_LANE_CHANGE}_${AGGRESSIVE}_${ASSERTIVE}_${LC_PROB}.txt &
+#
+#								let J=J+1
+#								if ((J == 20)); then
+#									wait
+#									let J=0
+#									echo "another batch"
+#								fi
+#							done
+#						done
+#					done
+#				done
+#			done
+#		done
+#	done
+#done
 
 
 wait 
