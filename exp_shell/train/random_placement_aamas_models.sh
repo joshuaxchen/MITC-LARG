f257267d2ea68fd4642bf79faa97eb0e5cd869e4
Yulin Zhang <ylzhang0008@gmail.com>
2021-10-20 11:02:57 -0500

    udpate shell

--- exp_shell/train/random_placement_aamas_models.sh
+++ exp_shell/train/random_placement_aamas_models.sh
@@ -6,9 +6,9 @@ export RAY_MEMORY_MONITOR_ERROR_THRESHOLD=0.8
 
 
 # Merge vehicle placement
-MERGE_INFLOW=300
+MERGE_INFLOW=200
 
-for AVP in 30
+for AVP in 10
 do
 	for MAIN_INFLOW in 2000 #1650 1850 2000 
 	do
@@ -16,29 +16,30 @@ do
 		let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
 		echo "Avp:${AVP}, Inflows:${MAIN_HUMAN_INFLOW} ${MAIN_RL_INFLOW} ${MERGE_INFLOW}"
 
-		python3 ${FLOW_DIR}/examples/rllib/multiagent_exps/random_modified_state_merge_placement_merge4_Collaborate_lrschedule.py \
+		python3 ${FLOW_DIR}/examples/rllib/multiagent_exps/random_placement_merge4_Merge4_Collaborate_lrschedule.py \
 		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
 		--to_probability \
-		--exp_folder_mark Avp${AVP}_Main${MAIN_INFLOW}_Merge${MERGE_INFLOW}
+		--cpu 30 \
+		--exp_folder_mark PostAAMAS_Avp${AVP}_Main${MAIN_INFLOW}_Merge${MERGE_INFLOW}
 	done
 done
 
 # Merge vehicle placement
-MERGE_INFLOW=200
-
-for AVP in 30
-do
-	for MAIN_INFLOW in 2000 #1650 1850 2000 
-	do
-		let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
-		let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
-		echo "Avp:${AVP}, Inflows:${MAIN_HUMAN_INFLOW} ${MAIN_RL_INFLOW} ${MERGE_INFLOW}"
-
-		python3 ${FLOW_DIR}/examples/rllib/multiagent_exps/random_modified_state_merge_placement_merge4_Collaborate_lrschedule.py \
-		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
-		--exp_folder_mark Avp${AVP}_Main${MAIN_INFLOW}_Merge${MERGE_INFLOW}
-	done
-done
+#MERGE_INFLOW=200
+#
+#for AVP in 30
+#do
+#	for MAIN_INFLOW in 2000 #1650 1850 2000 
+#	do
+#		let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
+#		let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
+#		echo "Avp:${AVP}, Inflows:${MAIN_HUMAN_INFLOW} ${MAIN_RL_INFLOW} ${MERGE_INFLOW}"
+#
+#		python3 ${FLOW_DIR}/examples/rllib/multiagent_exps/random_modified_state_merge_placement_merge4_Collaborate_lrschedule.py \
+#		--handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
+#		--exp_folder_mark Avp${AVP}_Main${MAIN_INFLOW}_Merge${MERGE_INFLOW}
+#	done
+#done
 
 wait
 
