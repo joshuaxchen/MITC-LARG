1f7e1560a2c3cfe80912035931107b9b47e6a390
Yulin Zhang <ylzhang0008@gmail.com>
2022-04-02 17:22:14 -0500

    add code for left lane policy

---
+++ exp_shell/evaluate/left_lane_timed_headway.sh
@@ -0,0 +1,179 @@
+HUMAN_DIR=/home/users/flow_user/ray_results/human_multiagent_highway_merge4_MOR_Collaborate_lrschedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4CollaborateMOR-v0_0573c_00000_0_2021-07-26_11-10-40
+
+TRAIN_DIR=/home/users/flow_user/ray_results/multiagent_yulin_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_6e090_00000_0_2021-08-05_18-04-04/
+
+TRAIN_DIR1=/home/users/flow_user/ray_results/multiagent_yulin_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_352ab_00000_0_2021-08-06_22-47-37/
+
+TRAIN_DIR2=${HOME}/ray_results/multiagent_yulin_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_7f52b_00000_0_2021-08-07_08-08-02/
+
+PRESET_0=${HOME}/ray_results/multiagent_yulin0_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_59fb3_00000_0_2021-08-21_09-16-18/
+
+PRESET_1=${HOME}/ray_results/multiagent_yulin1_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_5a9e2_00000_0_2021-08-21_09-16-19
+
+PRESET_2=${HOME}/ray_results/multiagent_yulin2_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_9abd7_00000_0_2021-08-21_09-18-06
+
+RL_LEFT_MODEL=${HOME}/ray_results/multiagent_yulin_rl_left_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_66b6b_00000_0_2021-08-27_23-35-41
+
+RL_RIGHT_MODEL=${HOME}/ray_results/multiagent_yulin_rl_right_lanechange_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_5e6dd_00000_0_2021-08-27_23-28-18
+
+RL_MODEL=${HOME}/ray_results/yulin_random_placement_multiagent_Even_Avp30_Main2000_Merge200_highway_merge4_Full_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_740c0_00000_0_2021-07-04_14-31-39
+
+RL_LEFT_OVAL=${HOME}/ray_results/multiagent_yulin_lanechange_left_oval_eta1_0.9_eta2_0.1/PPO_LeftLaneOvalHighwayPOEnvMerge4Collaborate-v0_d7ca7_00000_0_2021-11-12_21-00-22
+
+RL_LEFT_OVAL_MERGER=${HOME}/ray_results/multiagent_yulin_lanechange_left_oval_with_merger_eta1_0.9_eta2_0.1/PPO_LeftLaneOvalAboutToMergeHighwayPOEnvMerge4Collaborate-v0_b9c5c_00000_0_2021-11-12_22-11-07
+
+RL_LEFT_BASIC=${HOME}/ray_results/multiagent_yulin_lanechange_left_basic_five_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvCollaborate-v0_98fef_00000_0_2021-11-14_20-49-05/
+
+RL_LEFT_TIMED_HEADWAY=${HOME}/ray_results/multiagent_yulin_2000_1400_lanechange_left_basic_five_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvCollaborate-v0_7472b_00000_0_2022-04-02_00-19-22
+
+FLOW_DIR=${PWD}/../..
+#VISUALIZER=$FLOW_DIR/flow/visualize/new_rllib_visualizer.py
+VISUALIZER=$FLOW_DIR/flow/visualize/parallized_visualizer.py
+EXP_FOLDER=$FLOW_DIR/exp_results/lc_14000_8000
+
+
+
+echo "*************add python path to current direction***********"
+export PYTHONPATH="${PYTHONPATH}:${PWD}/../../"
+
+MAIN_INFLOW=2000
+MERGE_INFLOW=200
+AVP=10
+J=0
+
+mkdir ${EXP_FOLDER}
+RIGHT_MAIN_INFLOW=2000
+
+WORKING_DIR=$EXP_FOLDER
+mkdir ${WORKING_DIR}
+
+CHCKPOINT=500
+human_or_av=1
+render='no_render'
+horizon=14000
+
+for horizon in 2000 #2000 3000 4000 5000 6000 7000 8000 9000 10000 12000 14000
+do
+for RIGHT_MAIN_INFLOW in 2000 #1400 1600 1800 1900 2000 #2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
+do
+	for LEFT_MAIN_INFLOW in 1400 #1000 1200 1400 1600 #1200 1400 1600 # 1800 #1900 2000 2100 2200 # 1800 1900 2000 2100 2200 #1800 1900 #
+	do
+		for AVP_LEFT in 10 #20 30 40 #10 20 30 40 #200 400 600 800 # 200 400 600 800 # 200 400 600 800
+		do
+		    let RL_INFLOW_LEFT=LEFT_MAIN_INFLOW*${AVP_LEFT}/100
+		    let HUMAN_INFLOW_LEFT=LEFT_MAIN_INFLOW-RL_INFLOW_LEFT
+
+		    for AVP_RIGHT in 0 #20 30 40 #10 20 30 40 #200 400 600 800 # 200 400 600 800 # 200 400 600 800
+		    do
+			let RL_INFLOW_RIGHT=RIGHT_MAIN_INFLOW*${AVP_RIGHT}/100
+			let HUMAN_INFLOW_RIGHT=RIGHT_MAIN_INFLOW-RL_INFLOW_RIGHT
+			echo ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} 
+			echo ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT}
+
+			for SPEED_GAIN in 1 #0.2 0.4 0.6 0.8 1
+			do
+			    for ASSERTIVE in 5 #0.5 #5 #0.4 0.6 0.8 1
+			    do
+				for LC_PROB in -1
+				do
+				    # run AV policy
+                    #human_or_av=1
+                    if [[(human_or_av -eq 1)]]; then
+                        echo "run AV"
+                        python3 $VISUALIZER \
+                            $RL_LEFT_TIMED_HEADWAY \
+                            $CHCKPOINT \
+                            --agent_action_policy_dir $RL_MODEL \
+                            --seed_dir $FLOW_DIR \
+                            --lateral_resolution 3.2 \
+                            --render_mode ${render} \
+                            --human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT} \
+                            --rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+                            --human_lane_change 1 1 \
+                            --cpu 10 \
+                            --rl_lane_change 0 0 \
+                            --merge_inflow ${MERGE_INFLOW} \
+                            --speed_gain ${SPEED_GAIN} \
+                            --measurement_rate 8000 \
+                            --to_probability \
+                            --horizon ${horizon} \
+                            --assertive ${ASSERTIVE} \
+                            --lc_probability ${LC_PROB} \
+                        >> ${WORKING_DIR}/EVAL_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE}.txt &
+                            #--print_vehicles_per_time_step_in_file ${PWD}/figure/vehicles_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE} \
+                            #--print_metric_per_time_step_in_file  ${PWD}/figure/AV_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE} \
+                            #--print_inflow_outflow_var_in_file ${PWD}/log/${horizon} \
+                    fi
+
+                    #human_or_av=0
+                    if [[(human_or_av -eq 0)]]; then
+                        echo "run human"
+                        # run human baseline
+                        # add no lane changing vehicles at the right lane	
+                        let RL_INFLOW_RIGHT=0
+                        let RL_INFLOW_LEFT=0
+                        let NO_LANCHANGE_HUMAN_INFLOWS_ON_RIGHT=0
+                        let HUMAN_INFLOW_RIGHT=RIGHT_MAIN_INFLOW
+                        if [[(${AVP_RIGHT} != 0)]]; then # set the amount of vehicles to be non-lane-changing human drivers
+                            let NO_LANCHANGE_HUMAN_INFLOWS_ON_RIGHT=RIGHT_MAIN_INFLOW*${AVP_RIGHT}/100		
+                            let HUMAN_INFLOW_RIGHT=RIGHT_MAIN_INFLOW-NO_LANCHANGE_HUMAN_INFLOWS_ON_RIGHT
+                            echo "avp right is not 0"
+                        fi
+
+                        # add no lane changing vehicles at the left lane	
+                        let NO_LANCHANGE_HUMAN_INFLOWS_ON_LEFT=0
+                        let HUMAN_INFLOW_LEFT=LEFT_MAIN_INFLOW
+                        if [[(${AVP_LEFT} != 0)]]; then
+                            let NO_LANCHANGE_HUMAN_INFLOWS_ON_LEFT=LEFT_MAIN_INFLOW*${AVP_LEFT}/100		
+                            let HUMAN_INFLOW_LEFT=LEFT_MAIN_INFLOW-NO_LANCHANGE_HUMAN_INFLOWS_ON_LEFT
+                            echo "avp left is not 0"
+                        fi
+
+                        # run human baseline 
+                        python3 $VISUALIZER \
+                            $RL_LEFT_MODEL \
+                            $CHCKPOINT \
+                            --agent_action_policy_dir $RL_MODEL \
+                            --seed_dir $FLOW_DIR \
+                            --lateral_resolution 3.2 \
+                            --cpu 10 \
+                            --render_mode ${render} \
+                            --human_inflows ${HUMAN_INFLOW_RIGHT} ${HUMAN_INFLOW_LEFT} \
+                            --rl_inflows ${RL_INFLOW_RIGHT} ${RL_INFLOW_LEFT} \
+                            --human_lane_change 1 1 \
+                            --rl_lane_change 0 0 \
+                            --merge_inflow ${MERGE_INFLOW} \
+                            --speed_gain ${SPEED_GAIN} \
+                            --measurement_rate 8000 \
+                            --no_lanchange_human_inflows_on_right ${NO_LANCHANGE_HUMAN_INFLOWS_ON_RIGHT} \
+                            --no_lanchange_human_inflows_on_left ${NO_LANCHANGE_HUMAN_INFLOWS_ON_LEFT} \
+                            --to_probability \
+                            --assertive ${ASSERTIVE} \
+                            --lc_probability ${LC_PROB} \
+                            --horizon ${horizon} \
+                            >> ${WORKING_DIR}/EVAL_human_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE}.txt &
+                            #--run_random_seed 0 \
+                            #--print_vehicles_per_time_step_in_file ${PWD}/figure/human_vehicles_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE} \
+                            #--print_metric_per_time_step_in_file  ${PWD}/figure/human_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE} \
+                            #--print_inflow_outflow_var_in_file ${PWD}/log/${horizon} 
+                            #--print_metric_per_time_step_in_file  ${PWD}/figure/human_${RIGHT_MAIN_INFLOW}_${LEFT_MAIN_INFLOW}_${MERGE_INFLOW}_${AVP_RIGHT}_${AVP_LEFT}_${SPEED_GAIN}_${ASSERTIVE} \
+                        fi
+			let J=J+1
+		    if ((J == 4)); then
+			wait
+			let J=0
+			echo "another batch"
+		    fi
+				done
+			    done
+			done
+			done
+		done
+	done
+done
+done
+
+
+wait 
+source ~/notification_zyl.sh
+
--- flow/envs/multiagent/lanechange.py
+++ flow/envs/multiagent/lanechange.py
@@ -3,19 +3,136 @@ from flow.envs.multiagent import MultiAgentHighwayPOEnv
 from flow.core.rewards import desired_velocity, average_velocity
 import numpy as np
 from gym.spaces.box import Box
+from statistics import mean
+
+class LeftLaneHeadwayControlledMultiAgentEnv(MultiAgentHighwayPOEnv):
+    @property
+    def observation_space(self):
+        #See class definition
+        # basic 5 states + average speed of the vehicles on the right lane
+        # leader and follower at the other lane
+        # number of vehicles on the right lane that are about to move
+        return Box(-float('inf'), float('inf'), shape=(6,), dtype=np.float32)
+
+    @property
+    def action_space(self):
+        # the desired time headway of this agent
+        return Box(
+            low=0,
+            high=50,
+            shape=(1,),  # (4,),
+            dtype=np.float32)
+
+    def get_state(self):
+        states = super().get_state()
+        BEFORE_MERGE = 588
+        LOOK_AHEAD = 120
+        MAX_SPEED = 30
+        print("rl_ids", self.k.vehicle.get_rl_ids())
+        for rl_id in self.k.vehicle.get_rl_ids():
+            veh_ahead_on_the_right = list()
+            self_veh_x = self.k.vehicle.get_x_by_id(rl_id)
+            for other_veh_id in self.k.vehicle.get_ids():
+                lane_id = self.k.vehicle.get_lane(other_veh_id)
+                other_veh_x = self.k.vehicle.get_x_by_id(other_veh_id)
+                if lane_id == 0 and self_veh_x < other_veh_x and other_veh_x < BEFORE_MERGE and other_veh_x < self_veh_x + LOOK_AHEAD:  # 120
+                    other_veh_vel = self.k.vehicle.get_speed(other_veh_id)
+                    veh_ahead_on_the_right.append(other_veh_vel)
+            if len(veh_ahead_on_the_right) == 0:
+                mean_vel = MAX_SPEED
+            else:
+                mean_vel = mean(veh_ahead_on_the_right)
+            states[rl_id] = np.array(list(states[rl_id]) + [mean_vel])
+        print("states", states)
+        return states
+
+    def compute_reward(self, rl_actions, **kwargs):
+        if rl_actions is None:
+            return {}
+
+        rewards = {}
+        if "eta1" in self.env_params.additional_params.keys():
+            eta1 = self.env_params.additional_params["eta1"]
+            eta2 = self.env_params.additional_params["eta2"]
+        else:
+            eta1 = 0.9
+            eta2 = 0.1
+        reward1 = -0.1
+        reward2 = average_velocity(self)/300
+        reward = reward1 * eta1 + reward2 * eta2
+        for rl_id in self.k.vehicle.get_rl_ids():
+            rewards[rl_id] = reward
+        print(rewards)
+        return rewards
+
+    def idm_headway_to_accel(self, 
+        veh_id, 
+        desired_time_headway, 
+        v0=30, 
+        a=1, 
+        b=1.5, 
+        delta=4,
+        s0=2, 
+        time_delay=0.0, 
+        dt=0.1):
+
+        v = self.k.vehicle.get_speed(veh_id)
+
+        lane_id=self.k.vehicle.get_lane(veh_id)
+        # Fix the leader to be the leader on the same lane
+        lead_ids = self.k.vehicle.get_lane_leaders(veh_id)
+        lead_id = lead_ids[lane_id]
+
+        #h = env.k.vehicle.get_headway(self.veh_id)
+        # Fix the heaway to be the headway on the same lane accordingly
+        headways = self.k.vehicle.get_lane_headways(veh_id)
+        h = headways[lane_id]
+        T = desired_time_headway
+
+        # in order to deal with ZeroDivisionError
+        if abs(h) < 1e-3:
+            h = 1e-3
+
+        if lead_id is None or lead_id == '':  # no car ahead
+            s_star = 0
+        else:
+            lead_vel = self.k.vehicle.get_speed(lead_id)
+            s_star = s0 + max(
+                0, v * T + v * (v - lead_vel) /
+                (2 * np.sqrt(a * b)))
+        output_accel = self.a * (1 - (v / v0)**delta - (s_star / h)**2)
+        return output_accel
+
+    def _apply_rl_actions(self, rl_actions):
+        """See class definition."""
+        # in the warmup steps, rl_actions is None
+        if rl_actions:
+            for rl_id, actions in rl_actions.items():
+                desired_time_headway= actions[0]
+
+                # lane_change_softmax = np.exp(actions[1:4])
+                # lane_change_softmax /= np.sum(lane_change_softmax)
+                # lane_change_action = np.random.choice([-1, 0, 1],
+                #                                       p=lane_change_softmax)
+                accel = self.idm_headway_to_accel(rl_id, desired_time_headway)
+                self.k.vehicle.apply_acceleration(rl_id, accel)
+                # self.k.vehicle.apply_lane_change(rl_id, lane_change_action)
+
 
 class LeftLaneOvalHighwayPOEnvMerge4(MultiAgentHighwayPOEnv):
     @property
     def observation_space(self):
         #See class definition
         # basic 5 states
-        # leader and follower at the other lane 
+        # leader and follower at the other lane
         # number of vehicles on the right lane that are about to move
         return Box(-float('inf'), float('inf'), shape=(9,), dtype=np.float32)
 
     def _closest_vehicle(self, edge, lane, base_edge):
-        if edge == base_edge: return float('inf'), 0
-        if edge == '': return float('inf'), 0
+        if edge == base_edge:
+            return float('inf'), 0
+        if edge == '':
+            return float('inf'), 0
         veh = self.k.vehicle.get_ids_by_edge(edge)
         if len(veh) == 0:
             veh_pos = 0
@@ -34,20 +151,20 @@ class LeftLaneOvalHighwayPOEnvMerge4(MultiAgentHighwayPOEnv):
     def _merging_vehicle_backward_pass(self, edge, lane, base_edge, junctions):
         try:
             return min(self._merging_vehicle_backward_pass(e, l, base_edge, junctions) if e in junctions else self._closest_vehicle(e, l, base_edge)
-                    for e,l in self.k.network.prev_edge(edge, lane))
+                       for e, l in self.k.network.prev_edge(edge, lane))
         except ValueError:
             return float('inf'), 0
 
     def _merging_vehicle_forward_pass(self, edge, lane, base_edge, junctions):
         try:
             return min(self._merging_vehicle_forward_pass(e, l, base_edge, junctions) if e in junctions else self._merging_vehicle_backward_pass(e, l, base_edge, junctions)
-                    for e,l in self.k.network.next_edge(edge, lane))
+                       for e, l in self.k.network.next_edge(edge, lane))
         except ValueError:
             return float('inf'), 0
 
     def get_state(self):
         states = super().get_state()
-                
+
         for rl_id in states:
             edge_id = self.k.vehicle.get_edge(rl_id)
             lane = self.k.vehicle.get_lane(rl_id)
@@ -55,62 +172,64 @@ class LeftLaneOvalHighwayPOEnvMerge4(MultiAgentHighwayPOEnv):
             lead_ids = self.k.vehicle.get_lane_leaders(rl_id)
             follow_ids = self.k.vehicle.get_lane_followers(rl_id)
 
-            headways= self.k.vehicle.get_lane_headways(rl_id)
-            tailways= self.k.vehicle.get_lane_tailways(rl_id)
-            all_lane_ids=[i for i in range(0, len(headways))]
-            
-            other_lane_leader=None
-            shortest_headway=float('inf')
-            other_lane_leader_id=None
+            headways = self.k.vehicle.get_lane_headways(rl_id)
+            tailways = self.k.vehicle.get_lane_tailways(rl_id)
+            all_lane_ids = [i for i in range(0, len(headways))]
 
-            other_lane_follower=None
-            shortest_tailway=float('inf')
-            other_lane_follower_id=None
+            other_lane_leader = None
+            shortest_headway = float('inf')
+            other_lane_leader_id = None
+
+            other_lane_follower = None
+            shortest_tailway = float('inf')
+            other_lane_follower_id = None
 
             max_speed = 30.0
             max_length = 1000.0
 
             for other_lane in all_lane_ids:
-                if lane==other_lane:
+                if lane == other_lane:
                     continue
-                h=headways[other_lane] 
-                t=tailways[other_lane] 
+                h = headways[other_lane]
+                t = tailways[other_lane]
 
-                if h <shortest_headway:
-                    shortest_headway=h
-                    other_lane_leader_id=lead_ids[other_lane]
+                if h < shortest_headway:
+                    shortest_headway = h
+                    other_lane_leader_id = lead_ids[other_lane]
                 if t < shortest_tailway:
-                    shortest_tailway=t
-                    other_lane_follower_id=follow_ids[other_lane]
+                    shortest_tailway = t
+                    other_lane_follower_id = follow_ids[other_lane]
 
-            other_lane_leader_head=0
-            other_lane_leader_speed=0
-            other_lane_follower_tail=0
-            other_lane_follower_speed=0
+            other_lane_leader_head = 0
+            other_lane_leader_speed = 0
+            other_lane_follower_tail = 0
+            other_lane_follower_speed = 0
             if other_lane_leader_id in ["", None]:
-                other_lane_leader_head=max_length
-                other_lane_leader_speed=max_speed
+                other_lane_leader_head = max_length
+                other_lane_leader_speed = max_speed
             else:
-                other_lane_leader_speed=self.k.vehicle.get_speed(other_lane_leader_id)
-                other_lane_leader_head=shortest_headway
-            
+                other_lane_leader_speed = self.k.vehicle.get_speed(other_lane_leader_id)
+                other_lane_leader_head = shortest_headway
+
             if other_lane_follower_id in ["", None]:
-                other_lane_follower_tail=max_length
-                other_lane_follower_speed=max_speed
+                other_lane_follower_tail = max_length
+                other_lane_follower_speed = max_speed
             else:
-                other_lane_follower_tail=shortest_tailway
-                other_lane_follower_speed=self.k.vehicle.get_speed(other_lane_follower_id)
-            
-            states[rl_id] = np.array(list(states[rl_id]) + [other_lane_leader_speed/max_speed, other_lane_leader_head/max_length, other_lane_follower_speed/max_speed, other_lane_follower_tail/max_length])
+                other_lane_follower_tail = shortest_tailway
+                other_lane_follower_speed = self.k.vehicle.get_speed(other_lane_follower_id)
+
+            states[rl_id] = np.array(list(states[rl_id]) + [other_lane_leader_speed/max_speed, other_lane_leader_head /
+                                     max_length, other_lane_follower_speed/max_speed, other_lane_follower_tail/max_length])
         #print(states)
         return states
+
 
 class LeftLaneOvalAboutToMergeHighwayPOEnvMerge4(LeftLaneOvalHighwayPOEnvMerge4):
     @property
     def observation_space(self):
         #See class definition
         # basic 5 states
-        # leader and follower at the other lane 
+        # leader and follower at the other lane
         # number of vehicles on the right lane that are about to move
         return Box(-float('inf'), float('inf'), shape=(10,), dtype=np.float32)
 
@@ -123,33 +242,33 @@ class LeftLaneOvalAboutToMergeHighwayPOEnvMerge4(LeftLaneOvalHighwayPOEnvMerge4)
 
         max_length = 1000.0
         for rl_id in states:
-            edge_id=self.k.vehicle.get_edge(rl_id)
-            lane_id= self.k.vehicle.get_lane(rl_id)
-            rl_x=self.k.vehicle.get_x_by_id(rl_id)
+            edge_id = self.k.vehicle.get_edge(rl_id)
+            lane_id = self.k.vehicle.get_lane(rl_id)
+            rl_x = self.k.vehicle.get_x_by_id(rl_id)
             # find the closest vehicle that is about to merge to this lane
-            closest_x=float('inf')
-            closest_merger=None
+            closest_x = float('inf')
+            closest_merger = None
             for veh_id in self.k.vehicle.get_ids_by_edge(edge_id):
-                veh_lane_id= self.k.vehicle.get_lane(veh_id)
-                if lane_id<=veh_lane_id or veh_id==rl_id: # on the left or is current vehicle
+                veh_lane_id = self.k.vehicle.get_lane(veh_id)
+                if lane_id <= veh_lane_id or veh_id == rl_id:  # on the left or is current vehicle
                     continue
-                veh_x=self.k.vehicle.get_x_by_id(veh_id)
-                if veh_x<rl_x:
+                veh_x = self.k.vehicle.get_x_by_id(veh_id)
+                if veh_x < rl_x:
                     continue
-                lateral_pos=self.k.vehicle.get_lateral_lane_pos(veh_id) 
-                if lateral_pos>=1.0:
+                lateral_pos = self.k.vehicle.get_lateral_lane_pos(veh_id)
+                if lateral_pos >= 1.0:
                     # it is about to merge the left lane
-                    if closest_x>veh_x:
-                        closest_x=veh_x
-                        closest_merger=veh_id
-            closest_dist=closest_x-rl_x
+                    if closest_x > veh_x:
+                        closest_x = veh_x
+                        closest_merger = veh_id
+            closest_dist = closest_x-rl_x
             if closest_merger is None:
-                closest_dist=1
+                closest_dist = 1
             else:
-                closest_dist/=max_length
-            states[rl_id] = np.array(list(states[rl_id])+[closest_dist])                   
+                closest_dist /= max_length
+            states[rl_id] = np.array(list(states[rl_id])+[closest_dist])
         return states
-   
+
 
 class LeftLaneOvalHighwayPOEnvMerge4Collaborate(LeftLaneOvalHighwayPOEnvMerge4):
     def compute_reward(self, rl_actions, **kwargs):
@@ -165,7 +284,7 @@ class LeftLaneOvalHighwayPOEnvMerge4Collaborate(LeftLaneOvalHighwayPOEnvMerge4):
             eta2 = 0.1
         reward1 = -0.1
         reward2 = average_velocity(self)/300
-        reward  = reward1 * eta1 + reward2 * eta2
+        reward = reward1 * eta1 + reward2 * eta2
         for rl_id in self.k.vehicle.get_rl_ids():
             rewards[rl_id] = reward
         return rewards
@@ -185,8 +304,7 @@ class LeftLaneOvalAboutToMergeHighwayPOEnvMerge4Collaborate(LeftLaneOvalAboutToM
             eta2 = 0.1
         reward1 = -0.1
         reward2 = average_velocity(self)/300
-        reward  = reward1 * eta1 + reward2 * eta2
+        reward = reward1 * eta1 + reward2 * eta2
         for rl_id in self.k.vehicle.get_rl_ids():
             rewards[rl_id] = reward
         return rewards
-
